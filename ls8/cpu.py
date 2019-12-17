1c3bf1cba915fb073870e991a5f58ddfadd2c76c
Crawford <crawford.collins@gmail.com>
2019-12-17 15:34:45 -0600

    working on Tuesday

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,31 +7,30 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0b00000000]*7+0b00000001
+        self.ram =[]
         self.register = [None]*8
+        self.instruction_map ={
+                0b0001:"HLT",
+                0b0010:"LDI",
+                }
 
-    def load(self):
+    def load(self, f):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
-
+        with open(filename) as f:
+            for line in f:
+                 try:
+                     l = line.split("#")
+                     b = bin(l)
+                 except ValueError:
+                     continue
+                self.ram_write(address,b)
+                addres+=1
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
@@ -65,13 +64,31 @@ class CPU:
 
         print()
 
+    def _num_operands(binary):
+        operands = 0b1100000 & binary
+        operands = operands >>6
+        return operands
+    def _alu_operand(binary):
+        alu = 0b0010000 & binary
+        alu = alu>>5
+        return alu
+    def _set_pc(binary):
+        pc = 0b0001000 & binary
+        pc = pc >>4
+        return pc
+    def _instruction_identifier(binary):
+        ii = 0b00001111 & binary
+        
     def run(self):
         """Run the CPU."""
+        self.load()
+        print(self.ram)
         instruction_register = 0
         instruction = self.ram[instruction_register]
         if instruction == "HALT":
             pass
         else if instruction = "LDI":
+            break
 
 
 
