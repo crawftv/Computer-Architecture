9e056d101b5f6c33fd20b8ed8a6b503199e8fc6f
crawford <crawford.collins@gmail.com>
2019-12-18 18:58:46 -0600

    WEdneday complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -8,19 +8,21 @@ filename = sys.argv[1]
 class CPU:
     """Main CPU class."""
 
-    def __init__(self,filename):
+    def __init__(self, filename):
         """Construct a new CPU."""
         self.ram = [None] * 256
-        self.register = [None] * 8
+        self.register = [None] * 7 + [0xf4]
         self.instruction_map = {
             0b0001: "HLT",
             0b0111: "PRN",
             0b0010: "LDI",
-            0b0000:"NOOP",
+            0b0000: "NOOP",
+            0b0110: "POP",
+            0b0101: "PUSH"
         }
-        self.alu_map ={
-                0b0010: "MULT"
-                }
+        self.alu_map = {
+            0b0010: "MULT"
+        }
         self.filename = filename
 
     def load(self):
@@ -44,7 +46,7 @@ class CPU:
         """ALU operations."""
 
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
         # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -55,7 +57,82 @@ class CPU:
     def ram_write(self, address, value):
         self.ram[address] = value
 
-    #    def trace(self):
+    def _num_operands(self, binary):
+        operands = 0b1100000 & binary
+        operands = operands >> 6
+        return operands
+
+    def _alu_operand(self, binary):
+        binary = int(binary, base=2)
+        alu = 0b00100000 & binary
+        alu = alu >> 5
+        return alu
+
+    def _set_pc(self, binary):
+        pc = 0b0001000 & binary
+        pc = pc >> 4
+        return pc
+
+    def _instruction_identifier(self, binary):
+        ii = 0b00001111 & binary
+        return ii
+
+    def run(self):
+        """Run the CPU."""
+        self.load()
+        program_counter = 0x00
+        ii = None
+        while ii != "HLT":
+            instruction = self.ram[program_counter]
+            ii = int(instruction, 2)
+            ii = self._instruction_identifier(ii)
+            if self._alu_operand(instruction):
+                ii = self.alu_map[ii]
+                if ii == "MULT":
+                    reg_a = int(self.register[0], base=2)
+                    reg_b = int(self.register[1], base=2)
+                    self.register[0] = bin(reg_a*reg_b)
+                    program_counter += 2
+            else:
+                ii = self.instruction_map[ii]
+                if ii == "LDI":
+                    # import pdb
+                    # pdb.set_trace()
+                    register = int(self.ram[program_counter+1], base=2)
+                    immediate = self.ram[program_counter+2]
+
+                    self.register[register] = immediate
+                    program_counter += 2
+
+                elif ii == "PRN":
+                    reg_num = self.ram_read(program_counter+1)
+                    register = self.register[int(reg_num, base=2)]
+
+                    print(int(register, base=2))
+
+                    program_counter += 1
+                elif ii == "PUSH":
+                    self.register[-1] += -1
+                    reg_num = self.ram_read(program_counter+1)
+                    self.ram_write(
+                        self.register[-1],
+                        self.register[int(reg_num, base=2)]
+                    )
+
+                    program_counter += 1
+                elif ii == "POP":
+
+                    reg_num = int(self.ram_read(program_counter+1), base=2)
+                    stack_pointer = self.register[-1]
+
+                    self.register[reg_num] = self.ram[stack_pointer]
+
+                    self.register[-1] += 1
+                    program_counter += 1
+
+            program_counter += 1
+
+        #    def trace(self):
     #        """
     #        Handy function to print out the CPU state. You might want to call this
     #        from run() if you need help debugging.
@@ -75,52 +152,3 @@ class CPU:
     #            print(" %02X" % self.reg[i], end='')
     #
     #        print()
-
-    def _num_operands(binary):
-        operands = 0b1100000 & binary
-        operands = operands >> 6
-        return operands
-
-    def _alu_operand(self,binary):
-        binary = int(binary,base=2)
-        alu = 0b00100000 & binary
-        alu = alu >> 5
-        return alu
-
-    def _set_pc(self,binary):
-        pc = 0b0001000 & binary
-        pc = pc >> 4
-        return pc
-
-    def _instruction_identifier(self,binary):
-        ii = 0b00001111 & binary
-        return ii
-
-    def run(self):
-        """Run the CPU."""
-        self.load()
-        program_counter = 0
-        ii = None
-        while ii != "HLT":
-            instruction = self.ram[program_counter]
-            ii = int(instruction,2)
-            ii = self._instruction_identifier(ii)
-            if self._alu_operand(instruction):
-                ii = self.alu_map[ii]
-                if ii == "MULT":
-                    reg_a = int(self.register[0],base=2)
-                    reg_b = int(self.register[1],base=2)
-                    self.register[0] = bin(reg_a*reg_b)
-                    program_counter +=2
-            else:
-                ii = self.instruction_map[ii]
-                if ii == "LDI":
-                    register = self.ram_read(program_counter+1)
-                    immediate = self.ram[program_counter+2]
-                    self.register[int(register,base=2)] = immediate
-                    program_counter+=2
-                elif ii == "PRN":
-                    reg_num= self.ram_read(program_counter+1)
-                    register = int(self.register[int(reg_num,base=2)],base=2)
-                    print(register)
-            program_counter +=1
